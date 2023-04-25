776d02673775eb03dce08e99a8c40f3d5fb1c0f3
荣顶 <wangrongding@qq.com>
2023-04-25 14:03:42 +0800

    feat: 完成控制台 logo

--- packages/tiny-player/src/index.ts
+++ packages/tiny-player/src/index.ts
@@ -1,2 +1,7 @@
 import TinyPlayer from './script/player'
+
+console.log(
+  `${'\n'} %c ${TinyPlayer.title} v${TinyPlayer.version} ${'\n'}`,
+  `color: white; font-size: 18px; background: linear-gradient(45deg, #ff0000 0%, #0092ff 80%);`,
+)
 export default TinyPlayer
