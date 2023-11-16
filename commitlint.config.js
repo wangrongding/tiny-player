8af7e9f2db562388a5cac35dd769bf7cb9dba553
荣顶 <wangrongding@qq.com>
2023-11-16 23:22:54 +0800

    chore: 修改 commitlint 配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = { extends: ['cz'] }
+module.exports = {
+  extends: ['cz'],
+  rules: {},
+  helpUrl: `
+  
+  what-is-commitlint: 
+  https://github.com/conventional-changelog/commitlint/#what-is-commitlint
+  commit-message: 
+  https://www.authelia.com/contributing/guidelines/commit-message/`,
+}
